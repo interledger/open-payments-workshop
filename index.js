49c5c46076fd8d646df58823cd0327916aaa6c0b
Tymmmy <117268143+Tymmmy@users.noreply.github.com>
2025-09-23 01:23:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 // Open Payments Client Tutorial - Workshop
 // Goal make a peer to peer payment between two wallet addresses using test wallet
 
-// Open Payments Client => YOUR CLIENT WA
-// Sender => YOUR SENDER WA
-// Receiver => YOUR RECEIVER WA
+// Open Payments Client => Your Client
+// Sender => Sender WA
+// Receiver => Receiver WA
 
-(async () => {})();
+(async () => {
+  // 1. Fetch sender and receiver address
+  // 2. Get a grant for an incoming payment
+  // 3. Create an incoming payment for the receiver
+  // 4. Get a grant for a quote
+  // 5. Create a quote for the sender
+  // 6. Get a grant for an outgoing payment
+  // 7. (Go through user interaction)
+  // 8. Continue outgoing payment grant
+  // 9. Create outgoing payment for sender
+})();
